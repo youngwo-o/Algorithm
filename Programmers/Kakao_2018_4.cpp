da569de76858c7cbc3baaaaee3b8194fa4781e85
권영우 <ywoo093@naver.com>
2021-02-02 15:06:17 +0900

    뉴스 클러스터링
    map, 구현

---
+++ Programmers/Kakao_2018_4.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+#include <string>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+map<string, int> m1;
+map<string, int> m2;
+
+//다중집합 만들기
+void makeSet(string str, map<string,int>& m) {
+	for (int i = 0, len = str.size() - 1; i < len; ++i) {
+		//두글자 모두 알파벳인 경우만
+		if ('a' <= str[i] && str[i] <= 'z') {
+			if ('a' <= str[i + 1] && str[i + 1] <= 'z') {
+				string s = str.substr(i, 2);
+				if (m.count(s) == 0)
+					m.insert({ s, 1 });
+				else
+					m[s]++;
+			}
+		}
+	}
+}
+
+int solution(string str1, string str2) {
+	//소문자로 통일
+	transform(str1.begin(), str1.end(), str1.begin(), ::tolower);	
+	transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
+	
+	makeSet(str1, m1);
+	makeSet(str2, m2);
+
+	int a = 0, b = 0;
+	//m1 집합에서 교집합, 합집합의 개수 구하기
+	for (auto it = m1.begin(); it != m1.end(); ++it) {
+		string s = it->first;
+
+		if (m2.count(s) == 0)
+			b += it->second;
+		else {
+			a += min(it->second, m2[s]);
+			b += max(it->second, m2[s]);
+			m2.erase(s);
+		}
+	}
+	//합집합에 m1 집합에 없고 m2에만 있는 문자열 개수 추가
+	for (auto it = m2.begin(); it != m2.end(); ++it) {
+		string s = it->first;
+
+		b += it->second;
+	}
+
+	double j;
+	if (b != 0)
+		j = (double)a / b;
+	else
+		return 65536;
+
+	return j * 65536;
+}
+
+int main() {
+	cout << solution("FRANCE", "french");
+	return 0;
+}
