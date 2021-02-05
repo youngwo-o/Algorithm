d025a4df483d690a5672da0bc1da0764fa29b825
권영우 <ywoo093@naver.com>
2021-02-05 14:33:16 +0900

    n진수 게임
    진수 변환, string, 구현

--- Programmers/Kakao_2018_6.cpp
+++ Programmers/Kakao_2018_6.cpp
@@ -1,31 +1,19 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 //num을 n진수로 변환 -> string return
-string toNNumber(int n, int num) {
+string toNumber(int n, int num) {
 	string res = "";
-
-	if (num == 0 || num == 1) return to_string(num);
-
-	vector<int> tmp;
-	//num이 0이 될때까지 n으로 나눈 나머지 값 tmp에 저장
-	while (num >= 1) {
-		tmp.push_back(num % n);
-		num /= n;
-	}
-
-	//tmp 거꾸로
-	reverse(tmp.begin(), tmp.end());
-
 	string d = "0123456789ABCDEF";
 
-	//tmp값에 맞는 문자로 변환
-	for (auto t : tmp) {
-		res += d[t];
+	if (num == 0)
+		return "0";
+
+	while (num >= 1) {
+		int r = num % n;
+		res = d[r] + res;
+		num /= n;
 	}
 
 	return res;
@@ -35,8 +23,8 @@ string solution(int n, int t, int m, int p) {
 	string answer = "";
 
 	int cnt = 0, num = 0, idx = 0;
-	while (1) {
-		string s = toNNumber(n, num);
+	while (cnt != t) {
+		string s = toNumber(n, num);
 
 		for (int i = 0; i < s.length(); ++i) {
 			//튜브의 순서인 경우
@@ -45,14 +33,11 @@ string solution(int n, int t, int m, int p) {
 				//튜브가 말한 횟수 증가
 				cnt++;
 				//t번 말한 경우 break
-				if (cnt == t) break;
+				if (cnt == t) break ;
 			}
 			//순서 증가
 			idx++;
 		}
-
-		//t번 말한 경우 break
-		if (cnt == t) break;
 
 		//n진수로 변환할 숫자 증가
 		num++;
