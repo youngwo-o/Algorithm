82515688bf2a2b7dc275859960d4d2ae9a99792b
권영우 <ywoo093@naver.com>
2021-02-05 15:08:45 +0900

    파일명 정렬
    stable sort, stoi, string, 구현

--- Programmers/Kakao_2018_7.cpp
+++ Programmers/Kakao_2018_7.cpp
@@ -1,91 +1,38 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int strToInt(string s) {
-	int p = pow(10, s.length() - 1);
-	int res = 0;
-
+int findNumIdx(const string s) {
 	for (int i = 0; i < s.length(); ++i) {
-		res += ((s[i] - '0') * p);
-		p /= 10;
+		if (isdigit(s[i]))
+			return i;
 	}
+}
 
+int getNum(const string s) {
+	return stoi(s.substr(findNumIdx(s)));
+}
+
+string getHead(const string s) {
+	string res = s.substr(0, findNumIdx(s));
+	transform(res.begin(), res.end(), res.begin(), ::tolower);
 	return res;
 }
 
-bool cmp(vector<string> a, vector<string> b) {
-	transform(a[0].begin(), a[0].end(), a[0].begin(), ::tolower);
-	transform(b[0].begin(), b[0].end(), b[0].begin(), ::tolower);
+bool cmp(const string a, const string b) {
+	string head1 = getHead(a), head2 = getHead(b);
 
-	//header가 같으면, number 순
-	if (a[0] == b[0]) {
-		int aNum = strToInt(a[1]);
-		int bNum = strToInt(b[1]);
-
-		//number가 같으면, index 순(입력 시 주어진 순)으로
-		if (aNum == bNum)
-			return strToInt(a[3]) < strToInt(b[3]);
-		else  //number가 다르면, number 순
-			return aNum < bNum;
+	if (head1 == head2) {
+		int num1 = getNum(a), num2 = getNum(b);
+		
+		return num1 < num2;
 	}
-	else //header 다르면, header 사전순
-		return a[0] < b[0];
+	else
+		return head1 < head2;
 }
 
 vector<string> solution(vector<string> files) {
-	vector<string> answer;
+	stable_sort(files.begin(), files.end(), cmp);
 
-	vector<vector<string>> f;
-	for (int j = 0; j < files.size(); ++j) {
-		string file = files[j];
-
-		vector<string> hnt;
-		int idx = 0;
-		for (int i = 0; i < file.length(); ++i) {
-			if (hnt.size() == 0 && ('0' <= file[i] && file[i] <= '9')) {
-				hnt.push_back(file.substr(idx, i));
-				idx = i;
-			}
-			else if (hnt.size() == 1 && !('0' <= file[i] && file[i] <= '9')) {
-				hnt.push_back(file.substr(idx, i - idx));
-				idx = i;
-			}
-			else if(hnt.size() == 2)
-				hnt.push_back(file.substr(idx));
-		}
-
-		if (hnt.size() == 1) {
-			hnt.push_back(file.substr(idx));
-			hnt.push_back("");
-		}
-		else if (hnt.size() == 2) {
-			hnt.push_back("");
-		}
-
-		//C++ sort => quick sort (unstable)
-		//입력 시 주어진 순서로 정렬할때를 대비해서 현재 index 값 저장 (stable하게)
-		hnt.push_back(to_string(j));
-
-		f.push_back(hnt);
-	}
-
-	sort(f.begin(), f.end(), cmp);
-
-	for (auto s : f) {
-		string res = s[0] + s[1] + s[2];
-		cout << res << endl;
-		answer.push_back(res);
-	}
-
-	return answer;
-}
-
-int main() {
-	solution({ "aaa01", "AAA-01" });
-	return 0;
+	return files;
 }
