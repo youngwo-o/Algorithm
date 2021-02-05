1c67394cc4494db5ca6d9106c13383a8be601e4c
권영우 <ywoo093@naver.com>
2021-02-05 10:00:47 +0900

    비밀지도
    비트연산자, 진수변환, 구현

--- Programmers/Kakao_2018_2.cpp
+++ Programmers/Kakao_2018_2.cpp
@@ -1,33 +1,37 @@
-#include <iostream>
 #include <vector>
 #include <string>
-#include <cmath>
+//#include <cmath>
+//#include <algorithm>
 
 using namespace std;
 
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
-	vector<string> answer(n, "");
+	vector<string> answer;
 
 	for (int i = 0; i < n; ++i) {
 		int res = arr1[i] | arr2[i];
-
-		int p = pow(2, n - 1);
-		for(int j = 0; j < n; ++j) {
-			if (res >= p) {
+		string ans = "";
+		//int p = pow(2, n - 1);
+		for (int j = 0; j < n; ++j) {
+			//직접 나눠서 2진수 구하기
+			/*if (res >= p) {
 				res -= p;
 				answer[i] += '#';
 			}
 			else
 				answer[i] += ' ';
 
-			p /= 2;
+			p /= 2;*/
+
+			if (res % 2 == 0)
+				ans += '#';
+			else
+				ans += ' ';
+
+			//비트 연산자 활용!!
+			res = res >> 1;
 		}
 	}
 
 	return answer;
-}
-
-int main() {
-	
-	return 0;
 }
