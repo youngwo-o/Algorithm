0a9c387df644f15782ab92f32dfff846eb509c31
youngwo-o <ywoo093@naver.com>
2021-04-24 22:15:53 +0900

    Update 20057 (2).cpp

--- BOJ/20057 (2).cpp
+++ BOJ/20057 (2).cpp
@@ -6,7 +6,7 @@ using namespace std;
 int N;
 int board[MAX][MAX];
 int sum, r, c;
-int d[4][2] = { {0, -1}, {1, 0}, {0, 1}, {-1, 0} }; //¿Þ, ¾Æ, ¿À, À§
+int d[4][2] = { {0, -1}, {1, 0}, {0, 1}, {-1, 0} }; //ì™¼, ì•„, ì˜¤, ìœ„
 int dR[4][10] = { {-2, -1, -1, -1, 0, 1, 1, 1, 2, 0},
 				  {0, 1, 0, -1, 2, 1, 0, -1, 0, 1},
 				  {-2, -1, -1, -1, 0, 1, 1, 1, 2, 0},
@@ -42,72 +42,8 @@ void spread(int r, int c, int d) {
 	else board[nr][nc] += (sand - movedSand);
 }
 
-//void spread(int r, int c, int d) {
-//	int sand = board[r][c];
-//	if (sand == 0) return;
-//	board[r][c] = 0;
-//
-//	int movedSand = ((int)(sand * 0.02) + (int)(sand * 0.1) + (int)(sand * 0.07) + (int)(sand * 0.01)) * 2 + (int)(sand * 0.05);
-//	if (d == 0) { //¿ÞÂÊ ¹æÇâ
-//		for (int i = r - 2; i <= r + 2; i += 4) {
-//			if (i < 0 || i >= N) continue;
-//			board[i][c] += (sand * 0.02);
-//		}
-//		for (int i = r - 1; i <= r + 1; i += 2) {
-//			if (i < 0 || i >= N) continue;
-//			if (c - 1 >= 0) board[i][c - 1] += (sand * 0.1);
-//			board[i][c] += (sand * 0.07);
-//			if (c + 1 < N) board[i][c + 1] += (sand * 0.01);
-//		}
-//		if (c - 2 >= 0) board[r][c - 2] += (sand * 0.05);
-//		if (c - 1 >= 0) board[r][c - 1] += (sand - movedSand);
-//	}
-//	else if (d == 1) { //¾Æ·¡ÂÊ
-//		for (int j = c - 2; j <= c + 2; j += 4) {
-//			if (j < 0 || j >= N) continue;
-//			board[r][j] += (sand * 0.02);
-//		}
-//		for (int j = c - 1; j <= c + 1; j += 2) {
-//			if (j < 0 || j >= N) continue;
-//			if (r - 1 >= 0) 	board[r - 1][j] += (sand * 0.01);
-//			board[r][j] += (sand * 0.07);
-//			if (r + 1 < N) board[r + 1][j] += (sand * 0.1);
-//		}
-//		if (r + 2 < N) board[r + 2][c] += (sand * 0.05);
-//		if (r + 1 < N) board[r + 1][c] += (sand - movedSand);
-//	}
-//	else if (d == 2) { //¿À¸¥ÂÊ
-//		for (int i = r - 2; i <= r + 2; i += 4) {
-//			if (i < 0 || i >= N) continue;
-//			board[i][c] += (sand * 0.02);
-//		}
-//		for (int i = r - 1; i <= r + 1; i += 2) {
-//			if (i < 0 || i >= N) continue;
-//			if (c + 1 < N) board[i][c + 1] += (sand * 0.1);
-//			board[i][c] += (sand * 0.07);
-//			if (c - 1 >= 0) board[i][c - 1] += (sand * 0.01);
-//		}
-//		if (c + 2 < N) board[r][c + 2] += (sand * 0.05);
-//		if (c + 1 < N) board[r][c + 1] += (sand - movedSand);
-//	}
-//	else if (d == 3) { //À§ÂÊ
-//		for (int j = c - 2; j <= c + 2; j += 4) {
-//			if (j < 0 || j >= N) continue;
-//			board[r][j] += (sand * 0.02);
-//		}
-//		for (int j = c - 1; j <= c + 1; j += 2) {
-//			if (j < 0 || j >= N) continue;
-//			if (r + 1 < N) 	board[r + 1][j] += (sand * 0.01);
-//			board[r][j] += (sand * 0.07);
-//			if (r - 1 >= 0) board[r - 1][j] += (sand * 0.1);
-//		}
-//		if (r - 2 >= 0) board[r - 2][c] += (sand * 0.05);
-//		if (r - 1 >= 0) board[r - 1][c] += (sand - movedSand);
-//	}
-//}
-
 void move(int num) {
-	if (num % 2 == 0) { // Â¦¼ö -> ¿À¸¥ÂÊ, À§ÂÊ
+	if (num % 2 == 0) { // ì§ìˆ˜ -> ì˜¤ë¥¸ìª½, ìœ„ìª½
 		for (int i = 2; i < 4; ++i) {
 			int cnt = 0;
 			while (cnt < num) {
@@ -121,7 +57,7 @@ void move(int num) {
 			}
 		}
 	}
-	else { //È¦¼ö -> ¿ÞÂÊ, ¾Æ·¡ÂÊ
+	else { //í™€ìˆ˜ -> ì™¼ìª½, ì•„ëž˜ìª½
 		for (int i = 0; i < 2; ++i) {
 			int cnt = 0;
 			while (cnt < num) {
@@ -143,7 +79,7 @@ void solution() {
 	while (++cnt < N) {
 		move(cnt);
 	}
-	//¸Ç À­ÁÙ ÀÌµ¿
+	//ë§¨ ìœ—ì¤„ ì´ë™
 	for (int j = c - 1; j >= 0; --j) {
 		spread(r, j, 0);
 	}
